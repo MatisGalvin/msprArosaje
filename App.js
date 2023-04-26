4270465080fa40fa6b57f24ca3c34cd47be69053
Maxime BAUDOIN <maxime.baudoin@epsi.fr>
2023-04-26 10:00:48 +0200

    Edit CTACard

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { CTACard } from "./src/components/CTACard/CTACard";
 
@@ -14,22 +14,10 @@ export default function App() {
                 >
                     <Text>HEADER</Text>
                 </View>
-                <View style={{alignItems: 'center', justifyContent: 'center', padding: 16}}>
-                    <CTACard
-                        image={require('./assets/images/static/minipot.png')}
-                        title="Vous n’avez pas encore de plante !"
-                        content="Ajoutez une plante pour pouvoir la faire garder ou obtenir des conseils de botanistes professionnels"
-                        imageButton={require('./assets/images/static/plus.png')}
-                        textButton={"Ajouter votre première plante"}
-                    />
-                    <CTACard
-                        image={require('./assets/images/static/health.png')}
-                        title="J’ai besoin d’un conseil"
-                        content="Prenez une photo de votre plante et notre IA vous conseillera rapidement et efficacement pour régler le problème"
-                        imageButton={require('./assets/images/static/plus.png')}
-                        textButton={"Diagnostiquer ma plante"}
-                        handlePressButton={() => console.log("test")}
-                    />
+                <View style={{flex: 6}} >
+                    <ScrollView contentContainerStyle={{alignItems: 'center', justifyContent: 'start', padding: 16, flex: 1, gap: 50}}>
+
+                    </ScrollView>
                 </View>
                 <View
                     alignItems="center"
--- src/components/CTACard/CTACard.jsx
+++ src/components/CTACard/CTACard.jsx
@@ -2,27 +2,28 @@ import { useEffect, useRef } from "react";
 import { LargeButton } from "../LargeButton/LargeButton";
 import { Image, StyleSheet, Text, View } from "react-native";
 
-export const CTACard = ({image, title, content, imageButton, textButton, handlePressButton}) => {
+export const CTACard = ({image, title, content, imageButton, textButton, handlePressButton = () => {}}) => {
   
     return (
-        <View style={styles.container}>
+        <View style={styles.body}>
             { image && <Image source={image} alt="test" style={styles.image} /> }
-            <View space={3} p={6} alignSelf={"stretch"}>
-                {title && <Text textAlign={"center"} color={"gray.600"} size="md">{title}</Text>}
-                {content && <Text fontSize="sm" color={"gray.600"} textAlign={"center"}>{content}</Text>}
-                {/* {(imageButton && textButton) && <LargeButton image={imageButton} handlePress={handlePressButton}>{textButton}</LargeButton>} */}
+            <View style={styles.container}>
+                {title && <Text style={styles.title}>{title}</Text>}
+                {content && <Text style={styles.content}>{content}</Text>}
+                {(imageButton && textButton) && <LargeButton image={imageButton} handlePress={handlePressButton}>{textButton}</LargeButton>}
             </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    body: {
         alignItems: 'center',
         alignSelf: 'stretch',
         borderRadius: 12,
         backgroundColor: '#ffffff',
         marginTop: 24,
+        marginBottom: 15,
 
         shadowColor: '#000000',
         shadowOffset: {
@@ -33,9 +34,23 @@ const styles = StyleSheet.create({
         shadowRadius: 15
     },
     image: {
-        marginBottom: 4,
         marginTop: -50,
         width: 108,
         height: 108
+    },
+    container: {
+        gap: 10,
+        padding: 16
+    },
+    title: {
+        textAlign: 'center',
+        color: '#4B5563',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    content: {
+        color: '#4B5563',
+        textAlign: 'center',
+        fontSize: 14
     }
 });
