bc588512f880a0280d05cee3ba906b748b096e9e
Maxime BAUDOIN <maxime.baudoin@epsi.fr>
2023-04-27 16:20:27 +0200

    Mise en place Navigation Stack pour Notifications et Tab pour BottomBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,38 +4,11 @@ import { NotificationButton } from "../NotificationButton/NotificationButton";
 import { Separator } from "../Separator/Separator";
 import { ProfileButton } from "../ProfileButton/ProfileButton";
 import colors from "../../../colors";
+import { useNavigation } from "@react-navigation/native";
 
-import { createStackNavigator } from '@react-navigation/stack';
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-
-const Stack = createStackNavigator();
 
 export const Header = ({screenName}) => {
-
-    const navTheme = {
-        ...DefaultTheme,
-        colors: {
-            ...DefaultTheme.colors,
-            background: colors.background,
-        },
-    };
-
-    return (
-        <NavigationContainer theme={navTheme}>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-            >
-                <Stack.Screen name="Header">{() => <HeaderComponent screenName={screenName} />}</Stack.Screen>
-                {/* <Stack.Screen name="Notifications" component={Notifications} /> */}
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-
-}
-
-const HeaderComponent = ({screenName}) => {
+    const navigation = useNavigation();
 
     return <View style={styles.body}>
         <View style={[styles.container, {gap: 8}]}>
@@ -43,7 +16,7 @@ const HeaderComponent = ({screenName}) => {
             <Text style={styles.titleText}>{screenName}</Text>
         </View>
         <View style={[styles.container, {gap: 16}]}>
-            <NotificationButton unread={3} />
+            <NotificationButton unread={3} handlePress={() => navigation.navigate('Notifications')} />
             <Separator />
             <ProfileButton />
         </View>
--- src/components/NotificationButton/NotificationButton.jsx
+++ src/components/NotificationButton/NotificationButton.jsx
@@ -2,11 +2,11 @@ import { Text } from "react-native";
 import { Image, StyleSheet, TouchableOpacity, View } from "react-native"
 import colors from "../../../colors";
 
-export const NotificationButton = ({unread}) => {
+export const NotificationButton = ({unread, handlePress = () => {}}) => {
 
     // TODO : Gérer l'ouverture de screen Notifications
 
-    return <TouchableOpacity style={styles.body}>
+    return <TouchableOpacity style={styles.body} onPress={handlePress}>
         <Image source={require('../../../assets/images/static/bell.png')} style={styles.image} />
         {unread && <View style={styles.unreadContainer}><Text style={styles.unreadText}>{unread}</Text></View>}
     </TouchableOpacity>
