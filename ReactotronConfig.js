e1caeae53a5c4b1c1595b3c690a8ba3e5d095cf5
Antonin SIMON <antonin.simon@epsi.fr>
2023-07-20 21:56:37 +0200

    Reactotron implementation

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -2,6 +2,6 @@ import Reactotron from "reactotron-react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure({ host: process.env.MY_IP }) // controls connection & communication settings
+  .configure({ host: process.env.REACT_APP_MY_IP }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
